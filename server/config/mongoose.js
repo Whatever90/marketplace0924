ab97d9ab550556d59aad9855beb8483d7c2f3855
Whatever90 <bazetug@gmail.com>
2018-12-17 23:54:03 -0600

    mongoDB major fix

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
 var path = require('path');
-console.log('MongoDB is ready...')
+console.log('MongoDB is ready.')
 
 mongoose.connect('mongodb://localhost/marketplace');
 var models_path = path.join(__dirname, './../models');
