7fe7156c094fca02415b0a085acf53ac20b34b2e
BazT <bazetug@gmail.com>
2019-02-06 21:21:01 -0800

    home component sorting by category

--- ppm/src/app/home/home.component.ts
+++ ppm/src/app/home/home.component.ts
@@ -17,9 +17,11 @@ export class HomeComponent implements OnInit {
   loginUser = new LoginUser();
   error;
   errormessage;
+  hard_copy = [];
   products = [];
   product;
   length;
+  active_cat = 'All';
 
   constructor(private _taskService: TaskService, private _r: Router) {
     this.showAll();
@@ -28,13 +30,30 @@ export class HomeComponent implements OnInit {
     this._taskService.showMe(function (data, err) {
       if (data) {
         this.products = data;
+        this.hard_copy = data;
       }
       if (err) {
         console.log(err);
       }
     }.bind(this));
   }
-
+  selectCategory(cat){
+    this.active_cat = cat;
+    this.sortProducts(this.active_cat)
+  }
+  sortProducts(cat){
+    if(cat==='All'){
+      this.products = this.hard_copy;
+    }else{
+      let result = [];
+      for(let i in this.hard_copy){
+        if(this.hard_copy[i]['category']===cat){
+          result.push(this.hard_copy[i]);
+        }
+      }
+      this.products = result;
+    }
+  }
   ngOnInit() {
   }
 
